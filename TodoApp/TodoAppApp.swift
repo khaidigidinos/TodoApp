8c1b51e416808cca18c39b7f3e7ad8777c3a874c
Khai Nguyen <khainm@digidinos.com>
2022-03-20 23:01:57 +0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Khai Nguyen Minh on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
